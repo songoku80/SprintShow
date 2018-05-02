18376f41d67f5e475f4c2a2296644f42ea4837b4
Manel Merino <m.merino@epages.com>
2018-05-02 16:42:41 +0200

    Sprint 14 preparation

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -1,101 +1,71 @@
 define(function () {
     return [
+
     {
-        "id": "AD-8707",
-        "name": "Test Mockify 2.4",
-        "type": "Improvement",
-        "summary": "",
-        "priority": "Trivial",
-        "integrated": "7.23.0",
-        "platform": "ePages6",
-        "links": [
-            
-        ]
-    },
-    {
-        "id": "AD-8501",
-        "name": "Paypal Plus: Switch to 'Integrated signup' flow ",
-        "type": "Epic",
-        "summary": "",
-        "priority": "Major",
-        "integrated": "7.22.0",
-        "platform": "ePages6",
-        "links": [
-        "<a href='https://unitydemogb.barcelona.epages.systems/epages/unitydemogb.admin/?ViewAction=UnityMBO-ViewPaymentList&ObjectID=21334&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>DemoUnityGB Payment</a>",
-        "<a href='https://demounity.barcelona.epages.systems/epages/demounity.admin/?ViewAction=UnityMBO-ViewPaymentList&ObjectID=21099&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>DemoUnity Payment</a>"
-        ]
-    },
-    {
-        "id": "AD-8458",
-        "name": "Mollie: Unit test fails",
+        "id": "AD-8578",
+        "name": "FedEx: \"Real-Time calculation\" caused a SOAP_REQUEST_FAILED error",
         "type": "Bug",
-        "summary": "",
-        "priority": "Major",
-        "integrated": "7.23.0",
-        "platform": "ePages6",
-        "links": [
-            
-        ]
-    },
-    {
-        "id": "AD-8412",
-        "name": "Trusted Shops: Add support for multiple certificates in epages now",
-        "type": "Bug",
-        "summary": "",
-        "priority": "Major",
-        "integrated": "7.23.0",
-        "platform": "ePages6",
-        "links": [
-            
-        ]
-    },
-    {
-        "id": "AD-7763",
-        "name": "Shopping.com: phone no. is wrong",
-        "type": "Bug",
-        "summary": "",
-        "priority": "Minor",
-        "integrated": "7.23.0",
-        "platform": "ePages6",
-        "links": [
-            
-        ]
-    },
-    {
-        "id": "AD-8333",
-        "name": "Packlink & Shippo: Installation via the Appstore does not work in epages now",
-        "type": "Task",
         "summary": "",
         "priority": "Blocker",
         "integrated": "7.23.0",
         "platform": "ePages6",
-        "links": [
-            "<a href='https://demounity.barcelona.epages.systems/epages/demounity.admin/?ViewAction=UnityMBO-ViewAppStore&ObjectID=21099&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>DemoUnity AppStore</a>"
-        ]
+        "links": []
     },
     {
-        "id": "AD-8690",
-        "name": "Set Pull-Request-Status from Pipeline",
-        "type": "Technical Story",
+        "id": "UNITY-4516",
+        "name": "Paypal Express: Reactivate Instant update and review dependencies",
+        "type": "Task",
         "summary": "",
         "priority": "Major",
-        "integrated": "7.23.0",
+        "integrated": "7.24.0",
         "platform": "ePages6",
         "links": [
-            "<a href='https://epages.atlassian.net/browse/AD-8608?devStatusDetailDialog=pullrequest' target='_blank'>Jira Task</a>",
-            "<a href='http://teamred-jenkins.vm-intern.epages.com:8080/job/__INSTALL_Team_Infinity/' target='_blank'>Jenkins</a>",
-            "<a href='https://github.com/ePages-de/epagesj/pull/1801' target='_blank'>GitHub</a>"
-        ]
+            "<a href='https://demounitygb.teamred-06.vm-intern.epages.com/epages/demounitygb.admin/?ViewAction=MBO-ViewSettings&ObjectID=27590&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>PayPal Express Payment Settings (before)</a>",
+            "<a href='https://demounitygb.barcelona.epages.systems/epages/demounitygb.admin/?ViewAction=MBO-ViewSettings&ObjectID=21462&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>PayPal Express Payment Settings (after)</a>"
+	]
     },
     {
-        "id": "AD-8608",
-        "name": "Product Export: Content-Type mismatch between EP6 and EPJ",
+        "id": "UNITY-4199",
+        "name": "Rest API: If a newsletter app is uninstalled, the signup field is still shown",
+        "type": "Task",
+        "summary": "",
+        "priority": "Critical",
+        "integrated": "7.23.0",
+        "platform": "ePagesJ",
+        "links": [
+	    "<a href='https://demounity.barcelona.epages.systems/epages/demounity.admin/?ViewAction=MBO-ViewAppStore&categoryId=marketing_sales&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>App Store Now</a>"
+	]
+    },
+    {
+        "id": "AD-8688",
+        "name": "DHL: Price for domestic 1kg package is missing",
         "type": "Bug",
         "summary": "",
-        "priority": "Major",
+        "priority": "Blocker",
         "integrated": "7.23.0",
         "platform": "ePages6",
         "links": []
+    },
+    {
+        "id": "AD-8574",
+        "name": "Remove any mention of \"intraship\" (replace with \"versenden\")",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.24.0",
+        "platform": "ePages6",
+        "links": []
+    },
+    {
+        "id": "AD-8740",
+        "name": "Fix external machine installation",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.24.0",
+        "platform": "ePages6",
+        "links": [
+        ]
     }
 ]
 })
