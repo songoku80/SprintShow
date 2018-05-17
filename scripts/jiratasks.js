7c90cf2fb416d047015695c8532d489885f235cd
Manel Merino <m.merino@epages.com>
2018-05-17 11:25:55 +0200

    add jiratasks.js

---
+++ scripts/jiratasks.js
@@ -0,0 +1,71 @@
+define(function () {
+    return [
+    {
+        "id": "ESF-1649",
+        "name": "ESF: Fix the PayPal Plus tests for the pipeline ",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-8655",
+        "name": "Universal portal (epages now): own portals are always created for the \"default\" country",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+            "<a href='https://demounity.ocean-test.epages.systems/epages/demounity.admin/?ViewAction=UnityMBO-ViewUserDefinedPortals&ObjectID=20824&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Universal Product Portal (before)</a>",
+            "<a href='https://demounity.barcelona.epages.systems/epages/demounity.admin/?ViewAction=UnityMBO-ViewUserDefinedPortals&ObjectID=21113&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Universal Product Portal (current)</a>"
+        ]
+    },
+    {
+        "id": "AD-8625",
+        "name": "Trusted Shops: Removal of an invalid button in MBO",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.24.0",
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+            "<a href='https://ocean-test.epages.systems/epages/DemoShop.admin/?ObjectID=20399&ViewAction=MBO-ViewTrustedShopsCertificates&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Trusted Shops Settings (before)</a>",
+            "<a href='https://barcelona.epages.systems/epages/DemoShop.admin/?ObjectID=20688&ViewAction=MBO-ViewTrustedShopsCertificates&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Trusted Shops Settings (current)</a>"
+        ]
+    },
+    {
+        "id": "AD-8544",
+        "name": "PayPal Plus:Remark text is missing when paying via PayPal Plus",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-7496",
+        "name": "DHL: No tracking link is shown at mail",
+        "type": "Improvement",
+        "summary": "",
+        "priority": "Critical",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+            "<a href='img/DHL_Tracking_Link.png' target='_blank'>Email Example</a>"
+        ]
+    }
+]
+})
