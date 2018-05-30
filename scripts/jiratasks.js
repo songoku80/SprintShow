a3f0370dd9a99b03027243441e655ec064f5b134
Manel Merino <m.merino@epages.com>
2018-05-30 17:43:10 +0200

    infinity team sprint 16 tasks

---
+++ scripts/jiratasks.js
@@ -0,0 +1,43 @@
+define(function () {
+    return [
+    {
+        "id": "ESF-1664",
+        "name": "ESF build: fix problem when creating docker image",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-8783",
+        "name": "epagesj: Out of Memory on WhiteLabel",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Blocker",
+        "integrated": "7.24.0",
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+            "<a href='http://teamred-jenkins.vm-intern.epages.com:8080/job/build_dev_release/ws/build/reports/tests/classes/com.epages.metadata.product.category.CategoryInfoMapTest.html' target='_blank'>Test Results</a>"
+        ]
+    },
+    {
+        "id": "AD-8415",
+        "name": "RestAPI: category-tree temporarily unavailable after category renaming",
+        "type": "Bug",
+        "summary": "",
+        "priority": "Critical",
+        "integrated": "7.24.0",
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+            "<a href='http://teamred-jenkins.vm-intern.epages.com:8080/job/TEST_INSTALL_Team_Infinity/' target='_blank'>Jenkins TEST_INSTALL</a>"
+        ]
+    }
+]
+})
