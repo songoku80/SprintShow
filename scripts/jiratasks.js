123c99d5853427490f24243c6267dfa12dcf98d7
Manel Merino <m.merino@epages.com>
2018-06-13 15:31:13 +0200

    Infinity Review 17 tasks added

---
+++ scripts/jiratasks.js
@@ -0,0 +1,69 @@
+define(function () {
+    return [
+    {
+        "id": "AD-8804",
+        "name": "Prepare ESF Kickoff meeting",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-8799",
+        "name": "Research Spreedly Stripe: Send more customer data with each transaction",
+        "type": "Research",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-7577",
+        "name": "Remove Webmoney",
+        "type": "Task",
+        "summary": "",
+        "priority": "Minor",
+        "integrated": "7.25.0",
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-8749",
+        "name": "Automatic installation of \"Shell In A Box\" tool by ansible means from Jenkins",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": null,
+        "platform": "ePages6",
+        "relevance": "minor",
+        "links": [
+
+        ]
+    },
+    {
+        "id": "AD-8752",
+        "name": "Set allyouneed (former MeinPaket) to deprecated",
+        "type": "Task",
+        "summary": "",
+        "priority": "Major",
+        "integrated": "7.25.0",
+        "platform": "ePages6",
+        "relevance": "major",
+        "links": [
+
+        ]
+    }
+]
+})
