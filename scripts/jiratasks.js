2868879fa635f097ddeb65a078c69c68e258eea8
Manel Merino <m.merino@epages.com>
2018-06-14 11:43:20 +0200

    Add rate limit task and links

--- scripts/jiratasks.js
+++ scripts/jiratasks.js
@@ -40,19 +40,6 @@ define(function () {
         ]
     },
     {
-        "id": "AD-8804",
-        "name": "Prepare ESF Kickoff meeting",
-        "type": "Task",
-        "summary": "",
-        "priority": "Major",
-        "integrated": null,
-        "platform": "ePages6",
-        "relevance": "minor",
-        "links": [
-
-        ]
-    },
-    {
         "id": "AD-8752",
         "name": "Set allyouneed (former MeinPaket) to deprecated",
         "type": "Task",
@@ -66,6 +53,20 @@ define(function () {
             "<a href='https://unitydemo.teamred-05.vm-intern.epages.com/epages/unitydemo.admin/?ViewAction=UnityMBO-ViewProductPortals&ObjectID=21397&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Product Portals (Now)</a>",
             "<a href='https://teamred-05.vm-intern.epages.com/epages/DemoShop.admin/?ViewAction=MBO-ViewProductPortals&ObjectID=20610&ChangeAction=SaveLoginForm&Login=admin&Password=admin' target='_blank'>Product Portals (Base)</a>",
             "<a href='http://easy.vm-intern.epages.com:9000/component_measures/metric/coverage/history?id=DE_EPAGES%3A%3AProductPortal' target='_blank'>Code Coverage</a>"
+        ]
+    },
+    {
+        "id": "UNITY-4573",
+        "name": "Log the number of times the Rest call limit was used",
+        "type": "Task",
+        "summary": "",
+        "priority": "Critical",
+        "integrated": null,
+        "platform": "ePagesJ",
+        "relevance": "major",
+        "links": [
+            "<a href='img/Logback_pattern.png' target='_blank'>logback pattern</a>",
+            "<a href='img/Logback_log_info.png' target='_blank'>logback log example</a>",
         ]
     }
 ]
